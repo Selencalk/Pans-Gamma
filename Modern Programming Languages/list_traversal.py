225c2ef3e6cb750714fc67e2bf0f548259d04981
Selencalk <149872006+Selencalk@users.noreply.github.com>
2026-01-31 22:47:08 +0100

    Add list traversal in list_traversal.py
    Implement list traversal to print values in both forward and reverse order.

---
+++ Modern Programming Languages/list_traversal.py
@@ -0,0 +1,9 @@
+values = [10, 20, 30, 40]
+
+for i in range(len(values)):
+    print(values[i], end=" ")
+
+print("\n--------")
+
+for i in range(len(values) - 1, -1, -1):
+    print(values[i], end=" ")
