74d59f930c5d3ce6d6e44785ac02d5c3793497d3
Selencalk <selenak371@gmail.com>
2026-02-05 15:44:05 +0100

    Update matplotlib_grafik3.py

--- Modern Programming Languages/matplptlib_grafik3.py
+++ Modern Programming Languages/matplptlib_grafik3.py
@@ -1,12 +1,12 @@
 import random
 import matplotlib.pyplot as plt
 
-x = list(range(1, 16))
-y = [random.randint(0, 150) for _ in range(15)]
+x = list(range(1, 13))
+y = [random.randint(0, 100) for _ in range(12)]
 
 plt.bar(x, y)
-plt.xlabel("X Axis (1–15)")
-plt.ylabel("Random Values (0–150)")
+plt.xlabel("X Axis (1–12)")
+plt.ylabel("Random Values (0–100)")
 plt.title("Random Bar Chart")
 
 plt.show()
